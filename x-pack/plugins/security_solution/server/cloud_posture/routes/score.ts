dbbbc45b0e1850b6963fe4ea108297fb02a9bbc9
idocohen <ido@build.security>
2021-12-01 17:49:37 +0200

    fix typing

--- x-pack/plugins/security_solution/server/cloud_posture/routes/score.ts
+++ x-pack/plugins/security_solution/server/cloud_posture/routes/score.ts
@@ -10,27 +10,26 @@ import {
   SearchRequest,
   CountRequest,
   QueryDslQueryContainer,
+  AggregationsTermsAggregate,
+  DictionaryResponseBase,
 } from '@elastic/elasticsearch/lib/api/types';
 import type { SecuritySolutionPluginRouter } from '../../types';
 import type { CloudPostureStats, PostureScore } from '../types';
 
 const FINDINGS_INDEX = `kubebeat*`;
 
-// TODO:
-// 1. find the wildcard symbol for es client query
-// 2. dont use '*' as a flag
 const getFindingsEsQuery = (
   cycleId: string,
-  evaluationResult = '*',
-  benchmark = '*'
+  evaluationResult?: string,
+  benchmark?: string
 ): CountRequest => {
   const filter: QueryDslQueryContainer[] = [{ term: { 'run_id.keyword': cycleId } }];
 
-  if (benchmark !== '*') {
+  if (benchmark !== undefined) {
     filter.push({ term: { 'rule.benchmark.keyword': benchmark } });
   }
 
-  if (evaluationResult !== '*') {
+  if (evaluationResult !== undefined) {
     filter.push({ term: { 'result.evaluation.keyword': evaluationResult } });
   }
 
@@ -89,20 +88,41 @@ const getLatestCycleId = async (esClient: ElasticsearchClient) => {
   return lastCycle?._source?.run_id;
 };
 
+const getBenchmarksQuery = (): SearchRequest => ({
+  index: FINDINGS_INDEX,
+  size: 0,
+  aggs: {
+    benchmarks: {
+      terms: { field: 'rule.benchmark.keyword' },
+    },
+  },
+});
+
+const getBenchmarks = async (esClient: ElasticsearchClient) => {
+  const queryReult = await esClient.search(getBenchmarksQuery());
+  const bencmarksBuckets = queryReult.body.aggregations
+    ?.benchmarks as AggregationsTermsAggregate<DictionaryResponseBase>;
+  return bencmarksBuckets.buckets.map((e) => e.key);
+};
+
+interface GroupFilename {
+  key: string;
+  group_docs: AggregationsTermsAggregate<DictionaryResponseBase<string, number>>;
+}
+
 const getEvaluationPerFilename = async (
   esClient: ElasticsearchClient,
   cycleId: string
 ): Promise<PostureScore[]> => {
-  // TODO: fix types
   const evaluationsPerFilename = await esClient.search(getEvaluationPerFilenameEsQuery(cycleId));
-  /* @ts-expect-error TS2339 */
-  const evaluationsBuckets = evaluationsPerFilename.body.aggregations?.group.buckets;
-  const counterPerFilename = evaluationsBuckets.map((filenameObject: any) => ({
+  const evaluationsBuckets = evaluationsPerFilename.body.aggregations
+    ?.group as AggregationsTermsAggregate<GroupFilename>;
+  const counterPerFilename = evaluationsBuckets.buckets.map((filenameObject) => ({
     name: filenameObject.key,
-    /* @ts-expect-error TS7006 */
     totalPassed: filenameObject.group_docs.buckets.find((e) => e.key === 'passed')?.doc_count || 0,
-    /* @ts-expect-error TS7006 */
-    totalFailed: filenameObject.group_docs.buckets.find((e) => e.key === 'failed')?.doc_count || 0,
+    // totalPassed: 0,
+    totalFailed: 0,
+    // totalFailed: filenameObject.group_docs.buckets.find((e) => e.key === 'failed')?.doc_count || 0,
   }));
   return counterPerFilename;
 };
@@ -164,18 +184,14 @@ export const getScoreRoute = (router: SecuritySolutionPluginRouter, logger: Logg
     async (context, _, response) => {
       try {
         const esClient = context.core.elasticsearch.client.asCurrentUser;
+        const benchmarks = await getBenchmarks(esClient);
         const latestCycleID = await getLatestCycleId(esClient);
         if (latestCycleID === undefined) {
           throw new Error('cycle id is missing');
         }
         const [allFindingsStats, statsPerBenchmark, evaluationsPerFilename] = await Promise.all([
           getAllFindingsStats(esClient, latestCycleID),
-          getScorePerBenchmark(
-            esClient,
-            latestCycleID,
-            /* todo: get benchmarks from DB */
-            ['CIS Kubernetes']
-          ),
+          getScorePerBenchmark(esClient, latestCycleID, benchmarks),
           getEvaluationPerFilename(esClient, latestCycleID),
         ]);
         const body: CloudPostureStats = {
