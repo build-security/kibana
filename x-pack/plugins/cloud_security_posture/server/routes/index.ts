4d3ad9275ef680905aaee46a40bb8004d1337ab6
idocohen <ido@build.security>
2022-01-10 10:45:38 +0200

    fix TS errors

--- x-pack/plugins/cloud_security_posture/server/routes/index.ts
+++ x-pack/plugins/cloud_security_posture/server/routes/index.ts
@@ -8,7 +8,6 @@
 import type { IRouter, Logger } from '../../../../../src/core/server';
 import { defineGetStatsRoute } from './stats/stats';
 import { defineFindingsIndexRoute as defineGetFindingsIndexRoute } from './findings/findings';
-import { logger } from 'elastic-apm-node';
 
 export function defineRoutes(router: IRouter, logger: Logger) {
   defineGetStatsRoute(router, logger);
