74e794b4f02d7e370aec9a1584da95b5eb0aeabe
idocohen <ido@build.security>
2022-01-11 16:08:27 +0200

    add support in search bar

--- x-pack/plugins/cloud_security_posture/server/routes/findings/findings.ts
+++ x-pack/plugins/cloud_security_posture/server/routes/findings/findings.ts
@@ -10,6 +10,8 @@ import { SearchRequest, QueryDslQueryContainer } from '@elastic/elasticsearch/li
 import { schema as rt, TypeOf } from '@kbn/config-schema';
 import type { SearchSortOrder } from '@elastic/elasticsearch/lib/api/types';
 import type { IRouter } from 'src/core/server';
+import { fromKueryExpression, toElasticsearchQuery } from '@kbn/es-query';
+import { QueryDslBoolQuery } from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import { getLatestCycleIds } from './get_latest_cycle_ids';
 import { CSP_KUBEBEAT_INDEX_PATTERN, FINDINGS_ROUTE_PATH } from '../../../common/constants';
 
@@ -25,6 +27,59 @@ export interface FindingsOptions {
   fields?: string[];
 }
 
+const getTimeRangeFilter = (start?: string, end?: string): QueryDslQueryContainer[] => {
+  if (!!start && !!end) {
+    return [{ range: { '@timestamp': { gte: start } } }, { range: { '@timestamp': { lte: end } } }];
+  }
+  return [];
+};
+
+const buildLatestCycleFilter = (latestCycleIds?: string[]): QueryDslQueryContainer[] => {
+  if (!!latestCycleIds) {
+    const latestCycleFilter = latestCycleIds.map((latestCycleId) => ({
+      term: { 'run_id.keyword': latestCycleId },
+    }));
+    return latestCycleFilter;
+  }
+  return [];
+};
+
+const convertKqueryToElasticsearchQuery = (
+  filter?: string
+  // logger: Logger
+): QueryDslQueryContainer[] => {
+  let dslFilterQuery: QueryDslBoolQuery['filter'];
+  try {
+    dslFilterQuery = filter ? toElasticsearchQuery(fromKueryExpression(filter)) : [];
+    if (!Array.isArray(dslFilterQuery)) {
+      dslFilterQuery = [dslFilterQuery];
+    }
+  } catch (err) {
+    // TODO: uncomment after adding logger (task #2611) is merged
+    // logger.debug(`Invalid kuery syntax for the filter (${filter}) error:`, {
+    //   message: err.message,
+    //   statusCode: err.statusCode,
+    // });
+    throw err;
+  }
+  return dslFilterQuery;
+};
+
+const buildQueryRequest = (
+  queryParams: FindingsQuerySchema,
+  latestCycleIds?: string[]
+): QueryDslQueryContainer => {
+  const dateFilter = getTimeRangeFilter(queryParams.from, queryParams.to);
+  const filter = convertKqueryToElasticsearchQuery(queryParams.query);
+  const latestCycleIdsFilter = buildLatestCycleFilter(latestCycleIds);
+  filter.push(...latestCycleIdsFilter, ...dateFilter);
+  const query = {
+    bool: {
+      filter,
+    },
+  };
+  return query;
+};
 const getPointerForFirstDoc = (page: number, perPage: number): number =>
   page <= 1 ? 0 : page * perPage - perPage + 1;
 
@@ -34,6 +89,13 @@ const getSort = (sortField: string | undefined, sortOrder: string) =>
     : { sort: [{ '@timestamp': { order: sortOrder } }] };
 
 const getSearchFields = (fields: string | undefined) => (fields ? { _source: [fields] } : {});
+
+const buildOptionsRequest = (queryParams: FindingsQuerySchema): FindingsOptions => ({
+  size: queryParams.per_page,
+  from: getPointerForFirstDoc(queryParams.page, queryParams.per_page),
+  ...getSort(queryParams.sort_field, queryParams.sort_order),
+  ...getSearchFields(queryParams.fields),
+});
 
 const getFindingsEsQuery = (
   query: QueryDslQueryContainer,
@@ -45,27 +107,6 @@ const getFindingsEsQuery = (
     ...options,
   };
 };
-
-const buildQueryRequest = (latestCycleIds?: string[]): QueryDslQueryContainer => {
-  let filterPart: QueryDslQueryContainer = { match_all: {} };
-  if (!!latestCycleIds) {
-    const filter = latestCycleIds.map((latestCycleId) => ({
-      term: { 'run_id.keyword': latestCycleId },
-    }));
-    filterPart = { bool: { filter } };
-  }
-
-  return {
-    ...filterPart,
-  };
-};
-
-const buildOptionsRequest = (queryParams: FindingsQuerySchema): FindingsOptions => ({
-  size: queryParams.per_page,
-  from: getPointerForFirstDoc(queryParams.page, queryParams.per_page),
-  ...getSort(queryParams.sort_field, queryParams.sort_order),
-  ...getSearchFields(queryParams.fields),
-});
 
 export const defineFindingsIndexRoute = (router: IRouter): void =>
   router.get(
@@ -80,7 +121,7 @@ export const defineFindingsIndexRoute = (router: IRouter): void =>
 
         const latestCycleIds =
           request.query.latest_cycle === true ? await getLatestCycleIds(esClient) : undefined;
-        const query = buildQueryRequest(latestCycleIds);
+        const query = buildQueryRequest(request.query, latestCycleIds);
         const esQuery = getFindingsEsQuery(query, options);
         const findings = await esClient.search(esQuery);
         const hits = findings.body.hits.hits;
@@ -93,9 +134,12 @@ export const defineFindingsIndexRoute = (router: IRouter): void =>
   );
 
 export const findingsInputSchema = rt.object({
-  latest_cycle: rt.maybe(rt.boolean()),
   page: rt.number({ defaultValue: 1, min: 0 }),
   per_page: rt.number({ defaultValue: DEFAULT_FINDINGS_PER_PAGE, min: 0 }),
+  query: rt.maybe(rt.string()),
+  from: rt.maybe(rt.string()),
+  to: rt.maybe(rt.string()),
+  latest_cycle: rt.maybe(rt.boolean()),
   sort_field: rt.maybe(rt.string()),
   sort_order: rt.oneOf([rt.literal('asc'), rt.literal('desc')], { defaultValue: 'desc' }),
   fields: rt.maybe(rt.string()),
