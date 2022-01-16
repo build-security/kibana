77e77cd5cb8fe8f41a0f6671ef500d86c0058cee
Eyal Kraft <eyal.kraft@elastic.co>
2022-01-16 13:00:32 +0200

    fix merge

--- x-pack/plugins/cloud_security_posture/server/plugin.ts
+++ x-pack/plugins/cloud_security_posture/server/plugin.ts
@@ -13,17 +13,13 @@ import type {
   Logger,
 } from '../../../../src/core/server';
 import { createFindingsIndexTemplate } from './index_template/create_index_template';
-<<<<<<< HEAD
 import type {
   CspServerPluginSetup,
   CspServerPluginStart,
   CspServerPluginSetupDeps,
   CspServerPluginStartDeps,
 } from './types';
-=======
 import { ruleAssetType } from './saved_objects';
-import type { CspSetup, CspStart, CspPluginSetup, CspPluginStart } from './types';
->>>>>>> b350c816a15 (csp-rule asset type works)
 import { defineRoutes } from './routes';
 
 export class CspPlugin
