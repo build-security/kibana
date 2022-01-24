8bf1a57e24aa1f5a9cabff515a09fdc23cc88805
Or Ouziel <or.ouziel@elastic.co>
2022-01-24 19:21:46 +0200

    add useSearchSource hook

---
+++ x-pack/plugins/cloud_security_posture/public/common/utils/is_non_nullable.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+/**
+ * @example
+ * declare const foo: Array<string | undefined | null>
+ * foo.filter(isNonNullable) // foo is Array<string>
+ */
+export const isNonNullable = <T extends unknown>(v: T): v is NonNullable<T> =>
+  v !== null && v !== undefined;
