7f9e6d94113966972cd3a1aee166ee98cd9a9117
Or Ouziel <or.ouziel@elastic.co>
2022-01-24 19:21:10 +0200

    add useUrlQuery hook

--- x-pack/plugins/cloud_security_posture/public/common/navigation/query_utils.ts
+++ x-pack/plugins/cloud_security_posture/public/common/navigation/query_utils.ts
@@ -4,9 +4,10 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import { useEffect, useCallback, useMemo } from 'react';
 import { encode, decode, type RisonObject } from 'rison-node';
-import { stringify } from 'querystring';
 import type { LocationDescriptorObject } from 'history';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const encodeRison = (v: RisonObject): string | undefined => {
   try {
@@ -17,6 +18,8 @@ const encodeRison = (v: RisonObject): string | undefined => {
   }
 };
 
+// TODO: rison throws on EMPTY_QUERY,
+// maybe dont use that
 const decodeRison = <T extends unknown>(query: string): T | undefined => {
   try {
     return decode(query) as T;
@@ -30,7 +33,38 @@ const QUERY_PARAM_KEY = 'query';
 const EMPTY_QUERY = '';
 
 export const encodeQuery = (query: RisonObject): LocationDescriptorObject['search'] =>
-  stringify({ [QUERY_PARAM_KEY]: encodeRison(query) || EMPTY_QUERY });
+  `${QUERY_PARAM_KEY}=${encodeRison(query) || EMPTY_QUERY}`;
 
-export const decodeQuery = <T extends unknown>(search: string): Partial<T> | undefined =>
+export const decodeQuery = <T extends unknown>(search?: string): Partial<T> | undefined =>
   decodeRison<T>(new URLSearchParams(search).get(QUERY_PARAM_KEY) || EMPTY_QUERY);
+
+/**
+ * @description uses 'rison' to encode/decode a url query
+ */
+export const useUrlQuery = <T extends object>(getDefaultQuery: () => T) => {
+  const { push } = useHistory();
+  const loc = useLocation();
+
+  const urlQuery = useMemo(
+    () => ({ ...getDefaultQuery(), ...decodeQuery<T>(loc.search) }),
+    [getDefaultQuery, loc.search]
+  );
+
+  const setUrlQuery = useCallback(
+    (query: Partial<ReturnType<typeof getDefaultQuery>>) =>
+      push({
+        search: encodeQuery({ ...getDefaultQuery(), ...query }),
+      }),
+    [getDefaultQuery, push]
+  );
+
+  useEffect(() => {
+    if (loc.search) return;
+    setUrlQuery(getDefaultQuery());
+  }, [getDefaultQuery, loc.search, setUrlQuery]);
+
+  return {
+    urlQuery,
+    setUrlQuery,
+  };
+};
