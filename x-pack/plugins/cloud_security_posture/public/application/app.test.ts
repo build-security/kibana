66e2c26fb83c236602c5c38164614c93a5e3939a
Ido Cohen <ido.cohen@elastic.co>
2022-02-10 14:01:56 +0200

    solve conflicts3

--- x-pack/plugins/cloud_security_posture/public/application/app.test.ts
+++ x-pack/plugins/cloud_security_posture/public/application/app.test.ts
@@ -5,11 +5,7 @@
  * 2.0.
  */
 import Chance from 'chance';
-<<<<<<< HEAD
-import { createNavigationItemFixture } from '../test/fixtures/navigationItem';
-=======
 import { createNavigationItemFixture } from '../test/fixtures/navigation_item';
->>>>>>> main
 import { getRoutesFromMapping } from './app';
 
 const chance = new Chance();
