e71bcd31b4b1c3ced4bab53d91ed663afa2f59a8
Ido Cohen <ido.cohen@elastic.co>
2022-02-10 16:49:42 +0200

    fix TS errors

--- x-pack/plugins/cloud_security_posture/server/routes/findings/findings.ts
+++ x-pack/plugins/cloud_security_posture/server/routes/findings/findings.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import type { IRouter, Logger } from 'src/core/server';
+import type { IRouter } from 'src/core/server';
 import { SearchRequest, QueryDslQueryContainer } from '@elastic/elasticsearch/lib/api/types';
 import { schema as rt, TypeOf } from '@kbn/config-schema';
 import type { SortOrder } from '@elastic/elasticsearch/lib/api/types';
@@ -13,6 +13,7 @@ import { transformError } from '@kbn/securitysolution-es-utils';
 import { getLatestCycleIds } from './get_latest_cycle_ids';
 
 import { CSP_KUBEBEAT_INDEX_PATTERN, FINDINGS_ROUTE_PATH } from '../../../common/constants';
+import { CspAppContext } from '../../plugin';
 
 type FindingsQuerySchema = TypeOf<typeof findingsInputSchema>;
 
