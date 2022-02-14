74b6f5c09c9ac737573fda9e9cd39a28174268dd
Ido Cohen <ido.cohen@elastic.co>
2022-02-14 14:02:11 +0200

    working version with rebased branch

--- x-pack/plugins/cloud_security_posture/server/routes/index.ts
+++ x-pack/plugins/cloud_security_posture/server/routes/index.ts
@@ -9,10 +9,12 @@ import type { IRouter } from '../../../../../src/core/server';
 import { defineGetStatsRoute } from './stats/stats';
 import { defineGetBenchmarksRoute } from './benchmarks/benchmarks';
 import { defineFindingsIndexRoute as defineGetFindingsIndexRoute } from './findings/findings';
+import { defineSaveDataYamlRoute } from './configuration/save_data_yaml';
 import { CspAppContext } from '../plugin';
 
 export function defineRoutes(router: IRouter, cspContext: CspAppContext) {
   defineGetStatsRoute(router, cspContext);
   defineGetFindingsIndexRoute(router, cspContext);
   defineGetBenchmarksRoute(router, cspContext);
+  defineSaveDataYamlRoute(router, cspContext);
 }
